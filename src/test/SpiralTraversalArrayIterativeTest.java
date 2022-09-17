83c219c861ca01c51628302d0c6459850cc16e26
peymanbarca <p.yazdanian74@gmail.com>
2022-09-17 10:42:29 +0430

    add first unit test for recursive algorithm, but need to be completed

--- src/test/SpiralTraversalArrayIterativeTest.java
+++ src/test/SpiralTraversalArrayIterativeTest.java
@@ -2,14 +2,10 @@ package test;
 
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static SpiralTraversalArray.SpiralTraversalArrayRecursiveSolution.printInSpiralOrder;
+import static SpiralTraversalArray.SpiralTraversalArrayIterativeSolution.printInSpiralOrder;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -42,17 +38,143 @@ public class SpiralTraversalArrayIterativeTest {
                 { 13, 14, 15, 16 }
         };
 
-        int M = sampleArray.length, N = sampleArray[0].length;
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        printInSpiralOrder(sampleArray,0,0,M,N);
+        String result = printInSpiralOrder(sampleArray);
+
+        assertEquals("1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10",
+                result);
+
+    }
 
 
-        assertEquals("1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10,",
-                outContent.toString());
+    @Test
+    public void testSpiralPrintCase2() {
+
+
+        int sampleArray[][] = {
+                { 1, 2, 3,  },
+                { 4, 22, 6,  },
+                { 7, 222, 9,  },
+                { 10, 2222, 12 }
+        };
+
+        String result = printInSpiralOrder(sampleArray);
+
+        assertEquals("1,2,3,6,9,12,2222,10,7,4,22,222",
+                result);
+
+    }
+
+    @Test
+    public void testSpiralPrintCase3() {
+
+        int rows=6,columns=6;
+        int sampleArray[][] = new int[rows][rows];
+
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<columns;j++) {
+                sampleArray[i][j] = i+j+1;
+            }
+        }
+
+        String result = printInSpiralOrder(sampleArray);
+
+        assertEquals("1,2,3,4,5,6,7,8,9,10,11,10,9,8,7,6,5,4,3,2,3,4,5,6,7,8,9,8,7,6,5,4,5,6,7,6",
+                result);
+
+    }
+
+    // Negative sample
+    @Test
+    public void testSpiralPrintCase4() {
+
+        int rows=6,columns=6;
+        int sampleArray[][] = new int[rows][columns];
+
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<columns;j++) {
+                sampleArray[i][j] = i+j+1;
+            }
+        }
+
+        String result = printInSpiralOrder(sampleArray);
+
+        assertEquals(normalizeStringOfArray(sampleArray),
+                result);
+
+    }
+
+
+    @Test
+    public void testSpiralPrintCase5() {
+
+
+        int rows=4,columns=6;
+        int sampleArray[][] = new int[rows][columns];
+
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<columns;j++) {
+                sampleArray[i][j] = i+j+1;
+            }
+        }
+
+        String result = printInSpiralOrder(sampleArray);
+
+        assertEquals("1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,3,4,5,6,7,6,5,4",
+                result);
+
+    }
+
+
+    // Boundary test
+    @Test
+    public void testSpiralPrintCase6() {
+
+
+        int sampleArray[][] = new int[][]{};
+
+
+        String result = printInSpiralOrder(sampleArray);
+
+        assertEquals("",
+                result);
+
+    }
+
+    // Exceptional test
+    @Test
+    public void testSpiralPrintCase7() {
+
+
+        int sampleArray[][] = null;
+
+
+        String result = printInSpiralOrder(sampleArray);
+
+        assertEquals("",
+                result);
 
     }
 
 
 
+    @Test
+    public void testSpiralPrintCase8() {
+
+        int rows=8,columns=12;
+        int desiredElementsValue = 3;
+
+        int sampleArray[][] = new int[rows][columns];
+
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<columns;j++) {
+                sampleArray[i][j] = desiredElementsValue;
+            }
+        }
+
+        String result = printInSpiralOrder(sampleArray);
+
+        assertEquals(createStringAsNTimesOfNumber(rows*columns, desiredElementsValue),
+                result);
+
+    }
 }
